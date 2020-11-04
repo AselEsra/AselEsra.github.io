b973530553c9ce1408b054b97fa2747b9cbbcc0d
Asel Esra Ozyilmaz <esra.ozyilmaz@iaau.edu.kg>
2020-11-04 22:24:55 +0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function showPrice(){
+  alert('You can get the album only for 29.99$');
+}
+function printPhone(){
+  var num = document.getElementById("phoneNumber");
+  num.innerHTML="+996 706 197 714";
+}
